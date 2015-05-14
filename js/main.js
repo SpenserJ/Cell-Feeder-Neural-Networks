d4b950ba662ca5803e1d05cc4b9f5eec222f6525
Spenser Jones <hello@spenserj.com>
2015-05-14 15:07:25 -0600

    Use a single array of actors

--- js/main.js
+++ js/main.js
@@ -16,19 +16,25 @@ function calculateArea(radius) {
   var canvas = document.getElementById('game');
   var context = canvas.getContext('2d');
 
-  var cells = [], food = [];
-  cells.push(new Cell(10, canvas.width / 2, canvas.height / 2));
+  var actors = [];
+  actors.push(new Cell(10, getRandomArbitrary(0, canvas.width), getRandomArbitrary(0, canvas.height)));
+  actors.push(new Cell(10, getRandomArbitrary(0, canvas.width), getRandomArbitrary(0, canvas.height)));
 
   function tick() {
     var i;
-    for (i = 0; i < cells.length; i++) {
-      cells[i].look(food);
-      cells[i].tick();
+
+    actors = actors.filter(function (item) { return !item.isDead; });
+    var food = actors.filter(function (item) { return (item instanceof Food); });
+    var cells = actors.filter(function (item) { return (item instanceof Cell); });
+
+    for (i = food.length; i < 3; i++) {
+      actors.push(new Food(getRandomArbitrary(2, 10), getRandomArbitrary(0, canvas.width), getRandomArbitrary(0, canvas.height)));
     }
 
-    food = food.filter(function (item) { return !item.isDead; });
-    if (food.length === 0) {
-      food.push(new Food(getRandomArbitrary(2, 10), getRandomArbitrary(0, canvas.width), getRandomArbitrary(0, canvas.height)));
+    for (i = 0; i < actors.length; i++) {
+      var cell = actors[i];
+      if (typeof cell.look === 'function') { cell.look(food); }
+      cell.tick();
     }
 
     render();
@@ -39,12 +45,8 @@ function calculateArea(radius) {
 
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (i = 0; i < cells.length; i++) {
-      cells[i].draw(context);
-    }
-
-    for (i = 0; i < food.length; i++) {
-      food[i].draw(context);
+    for (i = 0; i < actors.length; i++) {
+      actors[i].draw(context);
     }
 
     requestAnimationFrame(tick);
